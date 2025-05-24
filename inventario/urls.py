207a4af18c17be915ab2f5033bd90a4b856ffbec
José Oporto <jose.oporto@example.com>
2025-05-24 02:51:54 -0400

    agregando bootstrap y css para los formularios

--- inventario/urls.py
+++ inventario/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('registro/', views.registro_inventario, name='registro'),
+    path('registro-usuario/', views.registro_usuario, name='registro_usuario'),
+    path('olvidar-contra/', views.olvidar_contra, name='olvidar_contra'),
     path('productos/', views.lista_productos, name='lista_productos'),
     path('productos/agregar/', views.agregar_producto, name='agregar_producto'),
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect
 from .models import Producto
 from .forms import ProductoForm
 
-# Create your views here.
-
 def index(request):
     return render(request, 'inventario/index.html')
 
@@ -11,20 +9,24 @@ def login_view(request):
     return render(request, 'inventario/login.html')
 
 def registro_inventario(request):
-    return render(request, 'inventario/registro.html')
+    return render(request, 'inventario/registro_item.html')
 
+def registro_usuario(request):
+    return render(request, 'inventario/registro_usuario.html')
+
+def olvidar_contra(request):
+    return render(request, 'inventario/olvidar_contra.html')
 
 def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, 'inventario/lista_productos.html', {'productos': productos})
-
 
 def agregar_producto(request):
     if request.method == 'POST':
         form = ProductoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('lista_productos')  # Redirige al listado después de guardar
+            return redirect('lista_productos')
     else:
         form = ProductoForm()
     return render(request, 'inventario/producto_form.html', {'form': form})
